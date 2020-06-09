d488104dd4aba31ed682e3280d94b99471c0a88f
Nonamerz <a.g.kolesnik@yandex.ru>
2020-06-09 21:26:09 +0300

    Initial Commit

---
+++ Contacts App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts App
+//
+//  Created by MacBook Pro on 09.06.2020.
+//  Copyright © 2020 Artem K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
